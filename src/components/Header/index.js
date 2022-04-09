4db709ca1cfd6d6806b4185711fe323b6aee4aa5
João Guilherme <joaoguilherme_pe@hotmail.com>
2022-04-09 15:14:30 -0300

    add new folder components

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <>
+            <h1>THIS IS MY HEADER</h1>
+        </>
+    )
+}
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Header from '../../components/Header'
+
 export default function Register(){
     return(
         <>
+        <Header />
         <h1>Register</h1>
         </>
     )
