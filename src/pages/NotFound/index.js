6da564871d1e1ae07f52e608d4a990dbd5157edf
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-04-16 15:45:31 -0300

    Page error 404 create

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import * as S from "./styles";
+import error404Image from "../../assets/error.svg"
+import { Link } from 'react-router-dom'
+
+import { ApiContext } from '../../contexts/apiContext'
 
 export default function Search() {
+    const { allProducts } = useContext(ApiContext)
+    const [products, setProducts] = useState([])
+
+    let productsHome = allProducts.slice(0, 8)
+
     return (
         <>
-
             <S.ContainerMain>
-                <p>Não tem página</p>
+                <img src={error404Image}/>
+                <p>
+                    Desculpe, mas a página que você está procurando não existe.
+                </p>
             </S.ContainerMain>
+
+            <S.TitleProducts>
+                <h1>Talvez você se interesse pelos seguintes produtos.</h1>
+            </S.TitleProducts>
+
+            <S.SomeProducts>
+                <ul>
+                    {productsHome.map((item) => (
+                        <li key={item.id}>
+                            <Link to={`/products/${item.id}`}>
+                                <img src={item.image} alt={`Imagem ${item.title}`} />
+                                <div>
+                                    <p>{item.title}</p>
+                                    <span>R${item.price}</span>
+                                    <p><strong>10</strong>x de <strong>R${(item.price / 10).toFixed(2)}</strong></p>
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </S.SomeProducts>
+
+            <S.ButtonSeeAllProducts>
+                <Link to="/products">Ver todos produtos</Link>
+            </S.ButtonSeeAllProducts>
         </>
     )
 }
--- src/pages/NotFound/styles.js
+++ src/pages/NotFound/styles.js
@@ -2,4 +2,116 @@ import styled from 'styled-components'
 import theme from '../../styles/theme'
 
 export const ContainerMain = styled.div`
+    display: block;
+    justify-content: center;
+    img {
+        display: flex;
+        justify-content: center;
+        margin: 50px auto;
+        width: 30%;
+        display: block;
+    }
+    p {
+        display: block;
+        text-align: center;
+    }
+`
+
+export const TitleProducts = styled.div`
+    margin: 2.5rem auto 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        text-transform: uppercase;
+        font-size: 1.5rem;
+        color: ${theme.gray2};
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+        @media (max-width: 550px){
+        text-align: center;
+        font-size: 1rem;
+    }
+}
+`
+
+export const SomeProducts = styled.div`
+    display: flex;
+    width: 82vw;
+    margin: 3rem auto 0 auto;
+    height: 100%;
+
+    ul {
+        display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 5rem;
+
+        li {
+            width: 225px;
+            height: 400px;
+            position: relative;
+            transition: 0.5s;
+            cursor: pointer;
+
+            a {
+                color: #000;
+            }
+
+            img {
+                width: 100%;
+                height: 60%;
+                &:hover {
+                    opacity: 1;
+                    filter: brightness(1);
+                }
+            }
+
+            div {
+                margin: 0 auto;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                margin-top: 10px;
+                
+                p {
+                    text-align: center;
+                    font-size: 0.9rem;
+                    text-transform: uppercase;
+                    font-weight: 500;
+                }
+
+                span {
+                    margin-top: 10px;
+                    font-weight: ${theme.fonts.weight.boldest};
+                    font-size: 1.3rem;
+                }
+            }
+        }
+    }
+`
+
+export const ButtonSeeAllProducts = styled.div`
+    display: flex;
+     a {
+        color: #fff;
+        text-transform: uppercase;
+        font-weight: ${theme.fonts.weight.boldest};
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 350px;
+        height: 40px;
+        background: #FF7D1B;
+        margin: 1rem auto 10rem auto;
+        &:hover {
+                filter: brightness(1.1);
+        }
+}
 `
