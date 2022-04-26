5af3bad310aa6249e246d7e8c841b33f7d50b2a3
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-04-26 18:36:48 -0300

    .env firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPuZ9emUtIKmMCvrkSJLw-GZIl0a8qmEs",
-  authDomain: "devshop-8a22c.firebaseapp.com",
-  projectId: "devshop-8a22c",
-  storageBucket: "devshop-8a22c.appspot.com",
-  messagingSenderId: "659905779370",
-  appId: "1:659905779370:web:16c3ac97ef41b8c3a7a2d3",
-  measurementId: "G-BL1CNQ2BB6"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSASING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 
